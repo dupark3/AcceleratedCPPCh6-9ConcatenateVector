80d7667b4f81e2118abe497cf1ac8b983e5cf4fb
Du Park <dupark3@gmail.com>
2018-02-23 14:52:39 -0500

    Added comments

--- concatenateStrings.cpp
+++ concatenateStrings.cpp
@@ -8,6 +8,7 @@
 std::string concatenateStrings (const std::vector<std::string>& vec){
     std::string ret;
 
+    // Look at each string, and use copy algorithm to copy char by char
     for (std::vector<std::string>::size_type i = 0; i != vec.size(); ++i)
         std::copy(vec[i].begin(), vec[i].end(), back_inserter(ret));
 
